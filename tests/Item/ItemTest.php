09fd1fad1b2d515e6708ad1f18ed502a1cecbc1c
Your Karim DAHOUMANE <krmdahoumane@gmail.com>
2021-12-29 23:33:14 +0100

    Added tests on Item Class

--- tests/Item/ItemTest.php
+++ tests/Item/ItemTest.php
@@ -10,4 +10,46 @@ class ItemTest extends TestCase {
     {
         parent::setUp();
     }
+
+    public function testItemWithValidInfos()
+    {
+        $item = new Item(
+            "Courses",
+            "Salade, Tomate, Oignon",
+            "2020-10-10 00:00:00"
+        );
+        $this->assertEquals("Courses", $item->getName());
+        $this->assertEquals("Salade, Tomate, Oignon", $item->getContent());
+        $this->assertEquals("2020-10-10 00:00:00", $item->getDateCreation());
+    }
+
+    public function testItemWithNonValidName()
+    {
+        $item = new Item(
+            "",
+            "Salade, Tomate, Oignon",
+            "2020-10-10 00:00:00"
+        );
+        $this->assertNotEquals("Courses", $item->getName());
+    }
+
+    public function testItemWithNonValidContent()
+    {
+        $item = new Item(
+            "Courses",
+            "",
+            "2020-10-10 00:00:00"
+        );
+        $this->assertNotEquals("Salade, Tomate", $item->getContent());
+    }
+
+    public function testItemWithNonValidDateCreation()
+    {
+        $item = new Item(
+            "Courses",
+            "Salade, Tomate",
+            ""
+        );
+        $this->assertNotEquals("2020-10-10 00:00:00", $item->getDateCreation());
+    }
 }
