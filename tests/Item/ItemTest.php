93c6b8eb181cbcab890a883a7bc24d2b8b956686
root <root@MSI.localdomain>
2021-12-29 21:40:40 +0100

    testNbItems undone

--- tests/Item/ItemTest.php
+++ tests/Item/ItemTest.php
@@ -1,8 +1,11 @@
 <?php
 declare(strict_types=1);
 
+use JetBrains\PhpStorm\NoReturn;
 use PHPUnit\Framework\TestCase;
 use App\Entity\Item;
+use function PHPUnit\Framework\assertFalse;
+use App\Services\ToDoList;
 
 class ItemTest extends TestCase {
 
@@ -19,6 +22,43 @@ class ItemTest extends TestCase {
         );
         $todayTimestamp = strtotime("now");
         $this->assertEquals($todayTimestamp, $item->getDateCreation()->getTimestamp());
+    }
+
+    public function testNbItems()
+    {
+        $todoList = new ToDoList();
+
+
+
+        for ($i=0; $i<4; $i++) {
+
+            $itemN = 'item' . $i;
+            $$itemN = new Item(
+                "$i","$i",
+                "202$i-01-01"
+            );
+            $todoList->add($itemN);
+
+        }
+
+
+
+
+
+        /*$expectedArray = [
+            0 => [
+                "faire la vaisselle",
+                "5mn"
+            ],
+            1 => [
+                ...
+            ],
+            ...
+        ];*/
+
+        dd($todoList->getItems());
+
+        assertTrue($todoList->getItems(), $expectedArray);
     }
 
 }
